2c877c952109a4f49f7a67733368690a958170ab
Adetule-ayomide <adetuleayomide@gmail.com>
2024-02-01 23:08:01 +0100

    javascript tutorial

--- UI-lib-project/src/index.js
+++ UI-lib-project/src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from "./ninja-ui/tooltip";
 import Dropdown from "./ninja-ui/dropdown";
 import Tabs from "./ninja-ui/tabs";
+import Snackbar from "./ninja-ui/snackbar";
 
 const tooltips = document.querySelector(".tooltip");
 
@@ -8,12 +9,21 @@ const tooltip = new Tooltip(tooltips);
 tooltip.init();
 
 // create dropdowns
-const dropdowns = document.querySelectorAll('.dropdown');
+const dropdowns = document.querySelectorAll(".dropdown");
 
-dropdowns.forEach(dropdown => {
+dropdowns.forEach((dropdown) => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
 
-const tabs = new Tabs(document.querySelector('.tabs'));
+const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
+
+// create snackbar instance
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  snackbar.show("you click me :)");
+});
